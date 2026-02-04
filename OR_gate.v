a4f14fe440c7f199355bf4ac79f6f965a2c28cd9
kathiravan247 <kathiravan.avlsi@gmail.com>
2026-02-04 12:03:56 +0530

    Add OR gate module and test bench

---
+++ OR_gate.v
@@ -0,0 +1,29 @@
+//Design
+module or_gate(
+  input a, b,
+  output y
+);
+  assign y = a | b;
+endmodule
+
+//Test bench
+module or_gatetb;
+  reg a, b;
+  wire y;
+  
+  or_gate Dut(
+    .a(a),
+    .b(b),
+    .y(y)
+  );
+  initial begin
+    $monitor("Time=%0t,a=%b,b=%b | y=%b", $time, a, b, y);
+    
+    a = 0; b = 0; #10;
+    a = 0; b = 1; #10;
+    a = 1; b = 0; #10;
+    a = 1; b = 1; #10;
+    
+    $finish;
+  end
+endmodule
